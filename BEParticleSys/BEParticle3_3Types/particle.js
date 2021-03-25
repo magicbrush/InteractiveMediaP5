9b47ab2a602b196d0efa18104a025f7f4e0df6f4
RuiminLyu <magicbrush@qq.com>
2021-03-25 22:58:21 +0800

    UpdateBEParticles

--- BEParticleSys/BEParticle3_3Types/particle.js
+++ BEParticleSys/BEParticle3_3Types/particle.js
@@ -118,12 +118,112 @@ function ParticleB(pos,pcolor,diameter,edgeWt)
 		else
 		{
 			var deltaP = createVector(EVec.x,EVec.y);
-			deltaP.mult(5*dt);
+			deltaP.mult(4*dt);
 			
 			deltaP.rotate(HALF_PI);
 			deltaP.mult(2*BValue/this.diameter);
 			var bias =  createVector(random(-1,1),random(-1,1));
 			bias.mult(2/this.diameter);
+			deltaP.add(bias);
+			p.add(deltaP);
+		}
+		if(p.x>=w)
+		{
+			p.x -=w;
+		}
+		if(p.x<0)
+		{
+			p.x+= w;
+		}
+		if(p.y>=h)
+		{
+			p.y-=h;
+		}
+		if(p.y<0)
+		{
+			p.y+= h;
+		}
+		
+		this.position = p;
+	}
+}
+
+
+function ParticleC(pos,pcolor,diameter,edgeWt)
+{
+	this.position = pos;
+	this.color = pcolor;
+	this.edgeWeight = edgeWt;
+	this.diameter = diameter;
+	this.render = function()
+	{
+		push();
+		fill(this.color);
+		strokeWeight(this.edgeWeight);
+		ellipse(
+			this.position.x,this.position.y,
+			this.diameter,this.diameter);
+		pop();
+		
+	}
+
+	this.step = function(B,E,dt)
+	{
+		var p = this.position;
+
+		var x = constrain(round(p.x),0,w-1);
+		var y = constrain(round(p.y),0,h-1);
+		//print("x:" + x + " y:" + y);
+		var BValue = B[x][y];
+		var EVec = E[x][y];
+
+
+		push();
+		colorMode(HSB,1,1,1,1);
+		var ch = hue(this.color);
+		var cs = saturation(this.color);
+		var cb = brightness(this.color);
+		var ca = alpha(this.color);
+		ch += 0.001*(BValue+atan2(EVec.y,EVec.x))*dt;
+		if(ch>1)
+		{
+			ch-=1;
+		}
+		else if(ch<0)
+		{
+			ch+=1;
+		}
+		var cr0 = color(ch,cs,cb,ca);
+		this.color = cr0;
+		pop();
+
+
+		for(var i=0;i<20;i++)
+		{
+			this.oneStep(BValue,EVec,dt);
+		}
+		
+	}
+
+	this.oneStep = function(BValue,EVec,dt)
+	{
+		var p = this.position;
+		if(EVec.mag()<0.0001)
+		{
+			p = createVector(random(0,w),random(0,w));
+		}
+		else
+		{
+			var deltaP = createVector(EVec.x,EVec.y);
+			deltaP.mult(dt);
+			push();
+			colorMode(HSB,1,1,1,1);
+			var ch = hue(this.color);
+			pop();
+			deltaP.rotate(TWO_PI*ch);
+			deltaP.mult(2*BValue/this.diameter);
+			var bias =  createVector(random(-1,1),random(-1,1));
+			bias.mult(0.5/this.diameter);
 			deltaP.add(bias);
 			p.add(deltaP);
 		}
@@ -155,18 +255,30 @@ function InitParticles(num)
 	{
 		var pos = createVector(random(0,w),random(0,h));
 		
-		if(random(0,1)<0.25)
+		var rvalue = random(0,1);
+		if(rvalue<0.5)
 		{
-			var psize = random(5,18);
+			var psize = random(10,18);
 			var pcr = 
-				color(random(100,200),random(200,255),random(200,255),random(20,40));
+				color(random(100,200),random(200,255),random(200,255),random(2,5));
 			P[i] = new ParticleA(pos,pcr,psize,0.0);
+		}
+		else if(rvalue>=0.5&&rvalue<0.6)
+		{
+			push();
+			var psize = random(2,5);
+			colorMode(HSB,1,1,1,1);
+			var pcr = 
+				color(random(0,1),random(0.7,0.9),random(0.2,0.5),random(0.7,0.9));
+			//var pcr = color(0,255,0,255);
+			P[i] = new ParticleC(pos,pcr,psize,0.01);
+			pop();
 		}
 		else
 		{
 			var psize = random(1,4);
 			var pcr = 
-				color(random(200,255),random(0,150),random(0,150),random(80,200));
+				color(random(200,255),random(0,150),random(0,150),random(120,200));
 			P[i] = new ParticleB(pos,pcr,psize,0.01);
 		}
 		
---
+++ BEParticleSys/BEParticle3_3Types/update.js
@@ -0,0 +1,155 @@
+
+// 计算E的改变量Delta of E
+var DeMax = 0.1;
+function CalDEbyB(B, DE, k)
+{
+	var dBdX = DifferentiateX(B);
+	var dBdY = DifferentiateY(B);
+
+	for(var c = 1;c<DE.length-1;c++)
+	{
+		for(var r =1;r<DE[c].length-1;r++)
+		{			
+			var dEx = dBdY[c][r]*k;
+			var dEy = -dBdX[c][r]*k;	
+			var DeVec = createVector(dEx,dEy);
+			DeVec.limit(DeMax);	
+			DE[c][r] = 	DeVec;	
+		}
+	}
+	return DE;
+}
+
+// update B
+function StepB(B)
+{
+	var B2 = CopyArray(B);
+	var tNow = GetTimeNow();
+	var hc = B.length/2;
+	var hr = B[0].length/2;
+	for(var c=0;c<B.length;c++)
+	{
+		for(var r=0;r<B[c].length;r++)
+		{
+			var c2 = a*(c-hc);
+			var r2 = b*(r-hr);
+			var radius = sqrt(c2*c2+r2*r2);
+			var theta = atan2(r2,c2);
+			//B2[c][r] = sin(radius+tNow)*sin(5*theta+tNow);
+			
+			var S0 = sin(2*radius+tNow);
+			var S1 = sin(5*theta+tNow);
+			B2[c][r] = S0*S1;				
+			if(c==5&&r==5)
+			{
+				//print("B[5][5]:" + B2[c][r] + " S0:" + S0 + " S1:"+S1 + " tNow:"+tNow);
+			}
+		}
+	}
+	return B2;
+}
+
+function StepB2(B)
+{
+	var B2 = CopyArray(B);
+	var tNow = GetTimeNow();
+	var hc = B.length/2;
+	var hr = B[0].length/2;
+	for(var c=0;c<B.length;c++)
+	{
+		for(var r=0;r<B[c].length;r++)
+		{
+			var c2 = a*(c-hc);
+			var r2 = b*(r-hr);
+			var radius = 2.5*sqrt(c2*c2+r2*r2);
+			var theta = atan2(r2,c2);
+			//B2[c][r] = sin(radius+tNow)*sin(5*theta+tNow);
+			
+			var S0 = sin(radius);
+			var S1 = sin(1*theta+tNow);
+			B2[c][r] = S0*S1;				
+			if(c==5&&r==5)
+			{
+				//print("B[5][5]:" + B2[c][r] + " S0:" + S0 + " S1:"+S1 + " tNow:"+tNow);
+			}
+		}
+	}
+	return B2;
+}
+
+// 对E进行更新：
+// update E by: E(t+dt) = E(t) + DE(t)*dt;
+function StepE(E,DE,dt)
+{
+	var E2 = CopyArray(E);
+	for(var i=0;i<E.length;i++)
+	{
+		for(var j=0;j<E[i].length;j++)
+		{
+			var e = E[i][j];
+			var de = DE[i][j];
+			de.mult(dt);
+			e.add(de);
+			E2[i][j] = e;
+		}
+	}
+	return E2;
+}
+
+function StepE2(E,DE,dt)
+{
+	var E2 = CopyArray(E);
+	for(var i=0;i<E.length;i++)
+	{
+		for(var j=0;j<E[i].length;j++)
+		{
+			var e = E[i][j];
+			var de = DE[i][j];
+			de.mult(5);
+			E2[i][j] = de;
+		}
+	}
+	return E2;
+}
+
+// 计算矢量场A的x方向微分 dA/dx
+function DifferentiateX(A)
+{
+	var D = CopyArray(A);
+
+	for(var c = 1;c<A.length-1;c++)
+	{
+		for(var r =1;r<A[c].length-1;r++)
+		{
+			cL = c-1;
+			cR = c+1;
+
+			AL = A[cL][r];
+			AR = A[cR][r];
+
+			D[c][r] = AR-AL;
+		}
+	}
+	return D;
+}
+
+// 计算矢量场A的y方向微分 dA/dy
+function DifferentiateY(A)
+{
+	var D = CopyArray(A);
+
+	for(var c = 1;c<A.length-1;c++)
+	{
+		for(var r =1;r<A[c].length-1;r++)
+		{
+			rU = r-1;
+			rB = r+1;
+
+			AU = A[c][rU];
+			AB = A[c][rB];
+
+			D[c][r] = AB-AU;
+		}
+	}
+	return D;
+}
